968ed7996b102fbf150db415c85ec8433c75bc5d
PomatoesBeep <dongerui1976@gmail.com>
2025-10-28 19:51:36 -0400

    E20 Processor

---
+++ sim.cpp
@@ -0,0 +1,295 @@
+/*
+CS-UY 2214
+Adapted from Jeff Epstein
+Starter code for E20 simulator
+sim.cpp
+*/
+
+#include <cstddef>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+#include <iomanip>
+#include <regex>
+#include <cstdlib>
+
+using namespace std;
+
+// Some helpful constant values that we'll be using.
+size_t const static NUM_REGS = 8;
+size_t const static MEM_SIZE = 1<<13;
+size_t const static REG_SIZE = 1<<16;
+
+/*
+    Loads an E20 machine code file into the list
+    provided by mem. We assume that mem is
+    large enough to hold the values in the machine
+    code file.
+
+    @param f Open file to read from
+    @param mem Array represetnting memory into which to read program
+*/
+void load_machine_code(ifstream &f, uint16_t mem[]) {
+    regex machine_code_re("^ram\\[(\\d+)\\] = 16'b(\\d+);.*$");
+    size_t expectedaddr = 0;
+    string line;
+    while (getline(f, line)) {
+        smatch sm;
+        if (!regex_match(line, sm, machine_code_re)) {
+            cerr << "Can't parse line: " << line << endl;
+            exit(1);
+        }
+        size_t addr = stoi(sm[1], nullptr, 10);
+        uint16_t instr = stoi(sm[2], nullptr, 2);
+        if (addr != expectedaddr) {
+            cerr << "Memory addresses encountered out of sequence: " << addr << endl;
+            exit(1);
+        }
+        if (addr >= MEM_SIZE) {
+            cerr << "Program too big for memory" << endl;
+            exit(1);
+        }
+        expectedaddr ++;
+        mem[addr] = instr;
+    }
+}
+
+/*
+    Prints the current state of the simulator, including
+    the current program counter, the current register values,
+    and the first memquantity elements of memory.
+
+    @param pc The final value of the program counter
+    @param regs Final value of all registers
+    @param memory Final value of memory
+    @param memquantity How many words of memory to dump
+*/
+void print_state(const uint16_t pc, const uint16_t regs[], uint16_t memory[], size_t memquantity) {
+    cout << setfill(' ');
+    cout << "Final state:" << endl;
+    cout << "\tpc=" <<setw(5)<< pc << endl;
+
+    for (size_t reg=0; reg<NUM_REGS; reg++)
+        cout << "\t$" << reg << "="<<setw(5)<<regs[reg]<<endl;
+
+    cout << setfill('0');
+    bool cr = false;
+    for (size_t count=0; count<memquantity; count++) {
+        cout << hex << setw(4) << memory[count] << " ";
+        cr = true;
+        if (count % 8 == 7) {
+            cout << endl;
+            cr = false;
+        }
+    }
+    if (cr)
+        cout << endl;
+}
+
+
+class E20Processor {
+public:
+    // Accept pointer to memory. Keeps types consistent with load_machine_code.
+    E20Processor(uint16_t memory[]) : mem(memory), programCounter(0), zeroFlag(false), halted(false) {
+        for (size_t i = 0; i < NUM_REGS; i++)
+            regs[i] = 0;
+    }
+
+    void run() {
+        while (!halted) {
+            // fetch instruction
+            uint16_t instr = static_cast<uint16_t>(mem[programCounter]);
+            execute(instr);
+            if (!halted) programCounter++;
+            if (programCounter >= MEM_SIZE) { 
+                cerr << "Program counter out of range: " << programCounter << endl;
+                halted = true;
+                break;
+            }
+        }
+    }
+
+    uint16_t const getPC() const { return programCounter; } 
+    uint16_t const *getRegs() const { return regs; } 
+
+private:
+    uint16_t regs[NUM_REGS];
+    uint16_t *mem;
+    uint16_t programCounter;
+    bool zeroFlag;
+    bool halted;
+
+    void execute(uint16_t instr) {
+        uint16_t opcode = (instr >> 13) & 0x7;
+
+        switch (opcode) {
+            case 0: { // add, sub, or, and, slt, jr
+                uint16_t r_a = (instr >> 10) & 0x7;
+                uint16_t r_b = (instr >> 7) & 0x7;
+                uint16_t r_dst = (instr >> 4) & 0x7;
+                uint16_t func = instr & 0xF;
+                if (r_dst == 0 && func != 8) return;
+
+                switch (func) {
+                    case 0: regs[r_dst] = regs[r_a] + regs[r_b]; break; // add
+                    case 1: regs[r_dst] = regs[r_a] - regs[r_b]; break; // sub
+                    case 2: regs[r_dst] = regs[r_a] | regs[r_b]; break; // or
+                    case 3: regs[r_dst] = regs[r_a] & regs[r_b]; break; // and
+                    case 4: regs[r_dst] = (regs[r_a] < regs[r_b]) ? 1 : 0; break; // slt
+                    case 8: programCounter = regs[r_a] - 1; break; // jr
+                    default: cerr << "Unknown R-type func: " << func << endl; break;
+                }
+
+                if (func != 8) zeroFlag = (regs[r_dst] == 0);
+                break;
+            }
+
+            
+            case 7: { // slti
+                uint16_t r_src = (instr >> 10) & 0x7;
+                uint16_t r_dst = (instr >> 7) & 0x7;
+                int16_t imm = instr & 0x7F;
+                if (r_dst == 0) break;
+                regs[r_dst] = (regs[r_src] < imm) ? 1 : 0;
+                break;
+            }
+            
+            case 4: { // lw 
+                uint16_t r_addr = (instr >> 10) & 0x7;
+                uint16_t r_dst = (instr >> 7) & 0x7;
+                uint16_t imm = instr & 0x7F;
+                if (r_dst == 0) break;
+                if (imm & 0x40) imm |= 0xFF80;
+                uint16_t addr = regs[r_addr] + imm;
+                if (addr >= MEM_SIZE) {
+                    cerr << "Memory access out of range at address " << addr << endl;
+                    halted = true;
+                    return;
+                }
+                regs[r_dst] = mem[addr];
+                break;
+            }
+            
+            case 5: { // sw
+                uint16_t r_addr = (instr >> 10) & 0x7;
+                uint16_t r_src = (instr >> 7) & 0x7;
+                int16_t imm = instr & 0x7F;
+                if (imm & 0x40) imm |= 0xFF80;
+                uint16_t addr = regs[r_addr] + imm;
+                if (addr >= MEM_SIZE) {
+                    cerr << "Memory access out of range at address " << addr << endl;
+                    halted = true;
+                    return;
+                }
+                mem[addr] = regs[r_src];
+                break;
+            }
+            
+            case 6: { // jeq
+                uint16_t r_a = (instr >> 10) & 0x7;
+                uint16_t r_b = (instr >> 7) & 0x7;
+                int16_t imm = instr & 0x7F;
+                if (imm & 0x40) { imm |= 0xFF80; }
+                if (regs[r_a] == regs[r_b]) {
+                    programCounter += imm;
+                }
+                break;
+            }
+            
+            case 1: { // addi
+                uint16_t r_src = (instr >> 10) & 0x7;
+                uint16_t r_dst = (instr >> 7) & 0x7;
+                int16_t imm = instr & 0x7F;
+                if (r_dst == 0) break;
+                if (imm & 0x40) { imm |= 0xFF80; } // sign extension
+                regs[r_dst] = regs[r_src] + imm;
+                zeroFlag = (regs[r_dst] == 0);
+                break;
+            }
+
+            case 2: { // j 
+                uint16_t imm = instr & 0x1FFF; 
+                if (imm == programCounter) { 
+                    halted = true;
+                    return;
+                }
+                programCounter = imm - 1;     
+                break;
+            }
+
+            case 3: { // jal 
+                uint16_t imm = instr & 0x1FFF;
+                regs[7] = programCounter + 1;
+                programCounter = imm - 1;
+                break;
+            }
+            
+            default:
+                cerr << "Unknown opcode: " << opcode << endl;
+                halted = true;
+                break;
+        }
+    }
+
+};
+
+
+/**
+    Main function
+    Takes command-line args as documented below
+*/
+int main(int argc, char *argv[]) {
+    /*
+        Parse the command-line arguments
+    */
+    const char *filename = nullptr;
+    bool do_help = false;
+    bool arg_error = false;
+    for (int i=1; i<argc; i++) {
+        string arg(argv[i]);
+        if (arg.rfind("-",0)==0) {
+            if (arg== "-h" || arg == "--help")
+                do_help = true;
+            else
+                arg_error = true;
+        } else {
+            if (filename == nullptr)
+                filename = argv[i];
+            else
+                arg_error = true;
+        }
+    }
+    /* Display error message if appropriate */
+    if (arg_error || do_help || filename == nullptr) {
+        cerr << "usage " << argv[0] << " [-h] filename" << endl << endl;
+        cerr << "Simulate E20 machine" << endl << endl;
+        cerr << "positional arguments:" << endl;
+        cerr << "  filename    The file containing machine code, typically with .bin suffix" << endl<<endl;
+        cerr << "optional arguments:"<<endl;
+        cerr << "  -h, --help  show this help message and exit"<<endl;
+        return 1;
+    }
+
+    ifstream f(filename);
+    if (!f.is_open()) {
+        cerr << "Can't open file "<<filename<<endl;
+        return 1;
+    }
+
+    // TODO: your code here. Load f and parse using load_machine_code
+
+    uint16_t memory[MEM_SIZE] = {0};
+    load_machine_code(f, memory);
+
+    // TODO: your code here. Do simulation.
+    E20Processor cpu(memory);
+    cpu.run();
+
+    // TODO: your code here. print the final state of the simulator before ending, using print_state
+
+    print_state(cpu.getPC(), cpu.getRegs(), memory, 128);
+
+    return 0;
+}
+//ra0Eequ6ucie6Jei0koh6phishohm9
